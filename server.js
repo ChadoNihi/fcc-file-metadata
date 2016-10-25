fd910f070f48ab9f14c3edd99343acc724fe3b3e
ChadoNihi <chadoishere@gmail.com>
2016-10-25 20:27:28 +0300

    done1

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express'),
       stylus = require('stylus'),
+      multer  = require('multer'),
       nib = require('nib'),
       pug = require('pug');
 
       app = express(),
+      upload = multer(),
       port = process.env.PORT || 5000,
       compile = (str, path)=> {
           return stylus(str)
@@ -23,9 +25,23 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+
 app.get('/', (req, res)=> {
   res.render('index');
 });
+
+app.post('/upload', upload.single('file'), (req, res)=> {
+  if (!req.file) {
+    res.status(400);
+    res.json({error: 'No file submitted!'});
+  } else {
+    res.json({
+      mimetype: req.file.mimetype,
+      sizebytes: req.file.size
+    });
+  }
+});
+
 
 app.listen(app.get('port'), ()=>{
   console.log(`The server is listening to port ${app.get('port')}`);
